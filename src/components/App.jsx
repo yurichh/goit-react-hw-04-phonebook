47c77b9b32b6ac3dec1e9de8681948977c6438c2
unknown <velidchukaleksey@gmail.com>
2023-12-11 20:38:13 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,34 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ContactList from './ContactList';
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import Notiflix from 'notiflix';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount() {
-    const localData = localStorage.getItem('contacts');
-    if (localData) this.setState({ contacts: JSON.parse(localData) });
-  }
+import React from 'react';
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+const App = () => {
+  const [contacts, setContacts] = useState(() =>
+    JSON.parse(localStorage.getItem('contacts'))
+  );
 
-  checkNameForRepeat = contactName => {
-    return this.state.contacts.some(
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const checkNameForRepeat = contactName => {
+    return contacts.some(
       ({ name }) => name.toLowerCase() === contactName.toLowerCase()
     );
   };
 
-  handleFilter = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
+  const handleFilter = ({ target: { value } }) => {
+    setFilter(value);
   };
 
-  handleAddContact = obj => {
-    /*Перевірка чи існує контакт*/
-    if (this.checkNameForRepeat(obj.name)) {
+  const handleAddContact = obj => {
+    if (checkNameForRepeat(obj.name)) {
       Notiflix.Notify.warning(`${obj.name} is already in contacts`, {
         position: 'center-top',
         distance: '50px',
@@ -46,42 +37,124 @@ export class App extends Component {
       });
       return;
     }
-
-    this.setState(prev => {
-      return {
-        contacts: [...prev.contacts, obj],
-      };
-    });
+    setContacts(prev => [...prev, obj]);
   };
 
-  handleDelete = id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(el => el.id !== id),
-    }));
+  const handleDelete = id => {
+    setContacts(prev => prev.filter(el => el.id !== id));
   };
 
-  getVisibleContacts = () => {
-    const normalizedFilter = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(c =>
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(c =>
       c.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  render() {
-    return (
-      <>
-        <h1 className="title">Phonebook</h1>
-        <ContactForm handleAddContact={this.handleAddContact} />
+  return (
+    <>
+      <h1 className="title">Phonebook</h1>
+      <ContactForm handleAddContact={handleAddContact} />
+      <section className="contacts-wrapper">
+        {(contacts.length === 0 && (
+          <h1 style={{ marginTop: 80, fontSize: 44 }}>
+            Ooops... No contacts here
+          </h1>
+        )) || (
+          <>
+            <h1 className="title">Contacts</h1>
+            <Filter handleFilter={handleFilter} />
+            <ContactList
+              contactsArray={getVisibleContacts()}
+              handleDelete={handleDelete}
+            />
+          </>
+        )}
+      </section>
+    </>
+  );
+};
 
-        <section className="contacts-wrapper">
-          <h1 className="title">Contacts</h1>
-          <Filter handleFilter={this.handleFilter} />
-          <ContactList
-            contactsArray={this.getVisibleContacts()}
-            handleDelete={this.handleDelete}
-          />
-        </section>
-      </>
-    );
-  }
-}
+export default App;
+
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+//   componentDidMount() {
+//     const localData = localStorage.getItem('contacts');
+//     if (localData) this.setState({ contacts: JSON.parse(localData) });
+//   }
+
+//   componentDidUpdate(_, prevState) {
+//     if (prevState.contacts !== this.state.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   checkNameForRepeat = contactName => {
+//     return this.state.contacts.some(
+//       ({ name }) => name.toLowerCase() === contactName.toLowerCase()
+//     );
+//   };
+
+//   handleFilter = ({ target: { name, value } }) => {
+//     this.setState({ [name]: value });
+//   };
+
+//   handleAddContact = obj => {
+//     /*Перевірка чи існує контакт*/
+//     if (this.checkNameForRepeat(obj.name)) {
+//       Notiflix.Notify.warning(`${obj.name} is already in contacts`, {
+//         position: 'center-top',
+//         distance: '50px',
+//         fontSize: '40px',
+//         width: '600px',
+//       });
+//       return;
+//     }
+
+//     this.setState(prev => {
+//       return {
+//         contacts: [...prev.contacts, obj],
+//       };
+//     });
+//   };
+
+//   handleDelete = id => {
+//     this.setState(prev => ({
+//       contacts: prev.contacts.filter(el => el.id !== id),
+//     }));
+//   };
+
+//   getVisibleContacts = () => {
+//     const normalizedFilter = this.state.filter.toLowerCase();
+//     return this.state.contacts.filter(c =>
+//       c.name.toLowerCase().includes(normalizedFilter)
+//     );
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <h1 className="title">Phonebook</h1>
+//         <ContactForm handleAddContact={this.handleAddContact} />
+
+//         <section className="contacts-wrapper">
+//           <h1 className="title">Contacts</h1>
+//           <Filter handleFilter={this.handleFilter} />
+//           <ContactList
+//             contactsArray={this.getVisibleContacts()}
+//             handleDelete={this.handleDelete}
+//           />
+//         </section>
+//       </>
+//     );
+//   }
+// }
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import Notiflix from 'notiflix';
-import { Component } from 'react';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  createContactObj = e => {
+const ContactForm = ({ handleAddContact }) => {
+  const [state, setState] = useState({ name: '', number: '' });
+
+  const createContactObj = e => {
     e.preventDefault();
 
-    /* Перевірка на заповнення полів */
-    if (!this.state.name || !this.state.number) {
+    if (!state.name || !state.number) {
       Notiflix.Notify.warning('Ooops... Something missed', {
         position: 'center-top',
         distance: '50px',
@@ -23,57 +19,124 @@ class ContactForm extends Component {
     }
 
     const newContactObj = {
-      name: this.state.name,
-      number: this.state.number,
+      name: state.name,
+      number: state.number,
       id: nanoid(),
     };
 
-    this.props.handleAddContact(newContactObj);
-    this.setState({ name: '', number: '' });
-
-    // e.target.form.reset();
+    handleAddContact(newContactObj);
+    setState({ name: '', number: '' });
   };
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
+  const handleChange = ({ target: { name, value } }) => {
+    setState(prev => ({ ...prev, [name]: value }));
   };
 
-  render() {
-    return (
-      <form action="submit" className="add-form">
-        <label htmlFor="name" className="add-label">
-          Name
-        </label>
-        <input
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          onChange={this.handleChange}
-          type="text"
-          name="name"
-          required
-          value={this.state.name}
-          className="add-input"
-        />
-        <label htmlFor="number" className="add-label">
-          Number
-        </label>
-        <input
-          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-          onChange={this.handleChange}
-          type="tel"
-          name="number"
-          required
-          value={this.state.number}
-          className="add-input"
-        />
-        <button
-          className="add-btn"
-          type="submit"
-          onClick={this.createContactObj}
-        >
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form action="submit" className="add-form">
+      <label htmlFor="name" className="add-label">
+        Name
+      </label>
+      <input
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        onChange={handleChange}
+        type="text"
+        name="name"
+        required
+        value={state.name}
+        className="add-input"
+      />
+      <label htmlFor="number" className="add-label">
+        Number
+      </label>
+      <input
+        pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+        onChange={handleChange}
+        type="tel"
+        name="number"
+        required
+        value={state.number}
+        className="add-input"
+      />
+      <button className="add-btn" type="submit" onClick={createContactObj}>
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 export default ContactForm;
+
+// class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+//   createContactObj = e => {
+//     e.preventDefault();
+
+//     /* Перевірка на заповнення полів */
+//     if (!this.state.name || !this.state.number) {
+//       Notiflix.Notify.warning('Ooops... Something missed', {
+//         position: 'center-top',
+//         distance: '50px',
+//         fontSize: '40px',
+//         width: '600px',
+//       });
+//       return;
+//     }
+
+//     const newContactObj = {
+//       name: this.state.name,
+//       number: this.state.number,
+//       id: nanoid(),
+//     };
+
+//     this.props.handleAddContact(newContactObj);
+//     this.setState({ name: '', number: '' });
+
+//     // e.target.form.reset();
+//   };
+//   handleChange = ({ target: { name, value } }) => {
+//     this.setState({ [name]: value });
+//   };
+
+//   render() {
+//     return (
+//       <form action="submit" className="add-form">
+//         <label htmlFor="name" className="add-label">
+//           Name
+//         </label>
+//         <input
+//           pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//           onChange={this.handleChange}
+//           type="text"
+//           name="name"
+//           required
+//           value={this.state.name}
+//           className="add-input"
+//         />
+//         <label htmlFor="number" className="add-label">
+//           Number
+//         </label>
+//         <input
+//           pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+//           onChange={this.handleChange}
+//           type="tel"
+//           name="number"
+//           required
+//           value={this.state.number}
+//           className="add-input"
+//         />
+//         <button
+//           className="add-btn"
+//           type="submit"
+//           onClick={this.createContactObj}
+//         >
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+
+// export default ContactForm;
